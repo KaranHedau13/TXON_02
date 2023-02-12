fdc2c452551d092c8697fb67798716809cb48307
KaranHedau13 <92622233+KaranHedau13@users.noreply.github.com>
2023-02-12 21:44:39 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function addValue(value)
+{
+    document.getElementById('result').value += value;
+}
+  
+function calculate() 
+{
+    document.getElementById('result').value = eval(document.getElementById('result').value);
+}
+  
+function clearValue() 
+{
+    document.getElementById('result').value = '';
+}
+  
